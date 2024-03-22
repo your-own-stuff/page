6bad1d23b7bf852cfe6c281a5ccf544675cfd325
Jan Ka <Jan-Ka@users.noreply.github.com>
2024-03-22 15:45:38 +0100

    fix:add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,41 @@
+<header
+	class="relative flex content-center items-center justify-center pb-32 pt-16"
+	style="min-height: 25vh;"
+>
+	<div class="header absolute top-0 h-full w-full bg-base-100 bg-cover bg-center">&nbsp;</div>
+</header>
+<section class="relative mt-24 bg-base-300 py-24">
+	<div class="absolute bottom-auto left-0 right-0 top-0 -mt-20 w-full" style="height:80px">
+		<svg
+			class="absolute bottom-0 overflow-hidden"
+			xmlns="http://www.w3.org/2000/svg"
+			preserveAspectRatio="none"
+			version="1.1"
+			viewBox="0 0 2560 100"
+			x="0"
+			y="0"
+			><polygon class="fill-current text-base-300" points="2560 0 2560 100 0 100"></polygon></svg
+		>
+	</div>
+	<div class="container mx-auto">
+		<div
+			class="relative z-10 -mt-64 flex flex-wrap justify-center rounded-lg bg-white px-12 py-16 shadow-xl"
+		>
+			<div class="w-full text-center lg:w-8/12">
+				<h1 class="text-9xl font-semibold text-primary">404</h1>
+				<div class="mb-4 flex w-full items-center justify-center">
+					<img
+						src="/img/exploded.webp"
+						alt="People looking at exploding machine"
+						class="w-1/2 rounded-lg"
+					/>
+				</div>
+				<h3 class="text-3xl font-semibold text-secondary">Page Not Found</h3>
+				<p class="mb-4 mt-4 text-lg leading-relaxed text-gray-600">
+					Looks like you looked for something that either isn't where it should or shouldn't be
+					where it is.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
